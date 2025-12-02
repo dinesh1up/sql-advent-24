aa1ecfed0104c4fe841496cdb0001a26cd3cdcf6
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-02 22:55:50 +0530

    day2.sql

---
+++ Day-2/day2.sql
@@ -0,0 +1,3 @@
+Santa wants to analyze which toys that were produced in his workshop have already been delivered to children....
+
+Question 1: Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
