4d7b0acd07e078cc890255463a6e1949c85e7d84
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-03 22:44:37 +0530

    day3.sql

--- Day-03/day3.sql
+++ Day-03/day3.sql
@@ -1,1 +1,26 @@
 (prank_id: INTEGER, target_name: VARCHAR, prank_description: VARCHAR, evilness_score: INTEGER, created_at: TIMESTAMP)
+
+
+
+WITH ranked AS (
+  SELECT
+    prank_id,
+    created_at,
+    target_name,
+    evilness_score,
+    prank_description,
+    ROW_NUMBER() OVER (
+      PARTITION BY target_name
+      ORDER BY evilness_score DESC, created_at DESC
+    ) AS rn
+  FROM grinch_prank_ideas
+)
+SELECT
+  prank_id,
+  created_at,
+  target_name,
+  evilness_score,
+  prank_description
+FROM ranked
+WHERE rn = 1
+ORDER BY target_name;
