ed2f1e125ef93e78675e5602fcfd69683895b58a
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-04 23:18:19 +0530

    day04.sql

--- Day-04/day04.sql
+++ Day-04/day04.sql
@@ -1,1 +1,3 @@
+(decoration_id: INT, decoration_name: VARCHAR, energy_cost_per_hour: DECIMAL)
 
+SELECT decoration_name, energy_cost_per_hour FROM hall_decorations order by energy_cost_per_hour LIMIT 5;
