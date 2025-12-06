276fcc4f9f2dcf93281f4ff3cdc38b77df2e3e89
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-06 18:10:46 +0530

    day05.sql

--- Day-05/day05.sql
+++ Day-05/day05.sql
@@ -1,2 +1,11 @@
 elves - (elf_id: INT, elf_name: VARCHAR)
 vacations - (elf_id: INT, start_date: DATE, return_date: DATE)
+
+
+SELECT
+  e.elf_name,
+  COALESCE(v.return_date, 'Still resting') AS return_date
+FROM elves e
+LEFT JOIN vacations v
+  ON e.elf_id = v.elf_id
+ORDER BY e.elf_id;
