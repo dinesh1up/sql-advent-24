760175c107b052c50e0b1ae41ff165f69fa85dfc
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-08 20:30:05 +0530

    day07.sql

--- Day-07/day07.sql
+++ Day-07/day07.sql
@@ -1,1 +1,9 @@
+Question - Frosty wants to know how many unique snowflake types were recorded on the December 24th, 2025 storm. Can you help him?
 
+  
+(flake_id: INT, flake_type: VARCHAR, fall_time: TIMESTAMP)
+
+
+SELECT COUNT(DISTINCT flake_type) AS unique_snowflake_types
+FROM snowfall_log
+WHERE fall_time = '2025-12-24';
