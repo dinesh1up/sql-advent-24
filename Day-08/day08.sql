89a8f03e02d202134dcd29ee92bf7c06cf50470a
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-09 17:40:19 +0530

    day08.sql

--- Day-08/day08.sql
+++ Day-08/day08.sql
@@ -1,2 +1,6 @@
 storage_trees - (item_name: VARCHAR, category: VARCHAR)
 storage_lights - (item_name: VARCHAR, category: VARCHAR)
+
+
+Question - Mrs. Claus is organizing the holiday storage room and wants a single list of all decorations — both Christmas trees and light sets. 
+Write a query that combines both tables and includes each item's name and category.
