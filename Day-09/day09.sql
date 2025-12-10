3191bd4d2c6f63d5481dbb1dd001a340eb7e8ee5
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-10 21:03:20 +0530

    day09.sql

--- Day-09/day09.sql
+++ Day-09/day09.sql
@@ -1,1 +1,6 @@
+Tables - tinsel_colors(tinsel_id, color_name); light_colors(light_id, color_name)
+
+Question - The elves are testing new tinsel–light combinations to find the next big holiday trend. 
+Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-").
+
 
