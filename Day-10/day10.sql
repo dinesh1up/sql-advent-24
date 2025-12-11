409306cbcb7e0e5a2ff81ba06f8d121466060e1e
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-11 23:35:27 +0530

    day10.sql

--- Day-10/day10.sql
+++ Day-10/day10.sql
@@ -1,1 +1,8 @@
 cookie_batches - (batch_id: INT, oven_id: INT, baking_time_minutes: DECIMAL)
+
+SELECT
+  oven_id,
+  ROUND(AVG(baking_time_minutes), 1) AS avg_baking_time_minutes
+FROM cookie_batches
+GROUP BY oven_id
+ORDER BY oven_id;
