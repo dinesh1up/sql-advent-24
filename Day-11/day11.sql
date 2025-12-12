cea1536177e7c3de3a1246220f55e44f915665e9
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-12 17:41:38 +0530

    day11.sql

--- Day-11/day11.sql
+++ Day-11/day11.sql
@@ -1,1 +1,5 @@
+winter_clothing - (item_id: INT, item_name: VARCHAR, color: VARCHAR)
 
+At the winter market, Cindy Lou is browsing the clothing inventory and wants to find all items with "sweater" in their name. 
+But the challenge is the color and item columns have inconsistent capitalization. 
+Can you write a query to return only the sweater names and their cleaned-up colors.
