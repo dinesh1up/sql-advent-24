2af9ec4cdf1a25666320875baf28df74f4d4af24
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-14 22:22:23 +0530

    day13.sql

--- Day-13/day13.sql
+++ Day-13/day13.sql
@@ -3,3 +3,5 @@ behavior_scores -  (record_id: INTEGER, child_name: VARCHAR, behavior_score: INT
 
 
 Santa's audit team is reviewing this year's behavior scores to find the extremes — write a query to return the lowest and highest scores recorded on the Naughty or Nice list.
+
+SELECT MIN(behavior_score) AS lowest_score, MAX(behavior_score) AS highest_score FROM behavior_scores;
