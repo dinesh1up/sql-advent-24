1c0d73bc3068696f073a60356d0e4716e174b013
Dinesh Kumar <115659686+dinesh1up@users.noreply.github.com>
2025-12-14 22:12:05 +0530

    day13.sql

---
+++ Day-13/day13.sql
@@ -0,0 +1,5 @@
+behavior_scores -  (record_id: INTEGER, child_name: VARCHAR, behavior_score: INTEGER)
+
+
+
+Santa's audit team is reviewing this year's behavior scores to find the extremes — write a query to return the lowest and highest scores recorded on the Naughty or Nice list.
